abc3d826e6a2757bd85796e9607b5a645bd9f1eb
Yaroslay <yaroslavstrychak@gmail.com>
2025-11-10 22:46:32 +0200

    last task done

--- src/exersice/Eighth_task.ts
+++ src/exersice/Eighth_task.ts
@@ -1,17 +1,18 @@
-// import axios from "axios";
-// interface Post {
-//     id: number;
-//     title: string;
-//     body: string;
-// }
-// async function fetchPosts() {
-//     const response = await axios.get(
-//         '<https://jsonplaceholder.typicode.com/posts>'
-//     );
-//     return response.data;
-// }
-// fetchPosts().then((posts) => {
-//     console.log(posts[0].title);
-// });
+import axios from "axios";
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+async function fetchPosts(): Promise<Post[]> {
+    const response = await axios.get<Post[]>(
+        '<https://jsonplaceholder.typicode.com/posts>'
+    );
+    return response.data;
+}
+fetchPosts().then((posts) => {
+    console.log(posts[0].title);
+});
 
 
